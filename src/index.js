5bb923d6af085956b60810222e1b0c4a295efd00
huutaidev <tainh030100@gmail.com>
2023-03-06 22:14:41 +0700

    Remove public/css folder

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const {engine} = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+//Static file
+app.use(express.static('./src/public/'));
+
+//HTTP Logger
+app.use(morgan('combined'));
+
+//Template engine
+app.engine('hbs', engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set("views", "./src/resources/views");
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port)
